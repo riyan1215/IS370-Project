5d05aa4379168990a68ccd601ea8be8cdeafbe38
riyan <riyan1215@gmail.com>
2025-04-05 16:04:34 +0300

    modified alot of things most importantly Added uni-cast

--- client.py
+++ client.py
@@ -1,22 +1,52 @@
 import socket
+import sys
+import threading
 
-HEADER=64
-PORT=5051
-FORMAT='UTF-8'
-DISCONNECT_MESSAGE="!DISCONNECT"
-SERVER="192.168.31.8"
-ADDR=(SERVER,PORT)
+HEADER = 256
+PORT = 5051
+FORMAT = 'UTF-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "127.0.0.1"
+ADDR = (SERVER, PORT)
 
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg):
-    message=msg.encode(FORMAT)
-    msg_length=len(message)
-    send_length=str(msg_length).encode(FORMAT)
-    send_length+=b' '*(HEADER-len(send_length))
-    client.send(send_length)
-    client.send(message)
 
-send("hello")
-send(DISCONNECT_MESSAGE)
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode(FORMAT)
+            if message:
+                if ":" in message:
+                    user, msg = message.split(":", 1)
+                    print(f"\r[{user}] {msg}\nEnter Message: ", end="")
+                else:
+                    print(f"\n{message}")
+        except Exception as e:
+            print(f"An error occurred while receiving message: {e}")
+            client.close()
+            break
+
+
+def send(msg):
+    client.send(msg.encode(FORMAT))
+
+
+user = input("Enter Username: ")
+send(user)
+message_back = client.recv(1024).decode(FORMAT)
+print(message_back)
+
+if message_back == "user unrecognized":
+    sys.exit()
+else:
+    receive_thread = threading.Thread(target=receive, daemon=True)
+    receive_thread.start()
+
+    while True:
+        msg = input("Enter Message: ")
+        if msg == "":
+            break
+        send(msg)
+    client.close()
--- server.py
+++ server.py
@@ -1,35 +1,64 @@
 import socket
 import threading
-HEADER=64
-PORT=5051
-SERVER="192.168.31.8"
-ADDR=(SERVER,PORT)
-FORMAT='UTF-8'
-DISCONNECT_MESSAGE="!DISCONNECT"
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(ADDR)
-def handle_client(conn,addr):
-    print(f"[NEW CONNECTION] {addr} connected")
-    connected=True
-    while connected:
-        msg_length=conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length=int(msg_length)
-            msg=conn.recv(msg_length).decode(FORMAT)
-            if msg==DISCONNECT_MESSAGE:
-                connected=False
-            else:
-             print(f"[{addr}] {msg}")
-    print(f'[{addr}] Disconnected')
 
+HEADER = 64
+PORT = 5051
+SERVER = "127.0.0.1"
+ADDR = (SERVER, PORT)
+FORMAT = 'UTF-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+clients = {}
+user_db = {
+    "riyan": "123",
+    "saad": "123",
+}
+
+
+def unicast(oguser, user, msg):
+    if user in clients:
+        msg = oguser + ':' + msg
+        print(msg)
+        clients[user].send(msg.encode(FORMAT))
+    else:
+        print("user is not connected")
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+    user = conn.recv(1024).decode()
+    if user.lower() in user_db:
+        conn.send(b"User has Logged in Successfully")
+        clients[user] = conn
+        while connected:
+            msg = conn.recv(1024).decode()
+            if msg[0] == "@":
+                user2, msg2 = msg[1:].split(" ", 1)
+                unicast(user, user2, msg2)
+            if not msg:
+                connected = False
+            else:
+                print(f"[{user}] {msg}")
+
+        print(f'[{user}] Disconnected')
+    else:
+        conn.send(b"user unrecognized")
+        print(f"{user} is Not Registered")
+    del clients[user]
     conn.close()
+
+
 def start():
     server.listen()
     print(f"[Listening] Server is Listening in {SERVER}")
     while True:
-        conn,addr=server.accept()
-        thread=threading.Thread(target=handle_client,args=(conn,addr))
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS]{threading.active_count()-1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
 print("[STARTING] server is starting...")
 start()
